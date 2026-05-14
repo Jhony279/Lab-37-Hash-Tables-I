452b180030a52fe12a886372f4d943a452baf1c0
Jhony279 <lordjhony732@gmail.com>
2026-05-14 14:41:30 -0700

    Working on sum_ascii function

--- lab-37-starter-1.cpp
+++ lab-37-starter-1.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+int sum_ascii(string);
 
 int main() {
     char a = 'A';
@@ -21,3 +22,13 @@ These targets are present in the dataset and can be used for testing:
 666D109AA22E
 E1D2665B21EA
 */
+
+int sum_ascii(string s) {
+    int sum = 0;
+
+    // Loop through each character 'c' in the string 'text'
+    for (char c : s) {
+        sum += (int) c;
+    }
+    return sum;
+}
